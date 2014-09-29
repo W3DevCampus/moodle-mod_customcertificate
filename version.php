18a259edbca75c5a42d0db34c7a5e7cceba19780
loise.fenoll <myuuu@live.fr>
2014-09-29 14:07:38 +0200

    Update Moodle version requires and plugin version

--- version.php
+++ version.php
@@ -9,8 +9,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2013070501; // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2011120500;  // Requires this Moodle version
+$module->version  = 2014092900; // The current module version (Date: YYYYMMDDXX)
+$module->requires = 2013051405;  // Requires this Moodle version
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $module->release  = '1.0.0';       // Human-friendly version name
 //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
